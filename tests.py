79ebd5c6a586c443dc9a6d72f0d492e9059f206f
tibut <marcel.wallschlaeger@tu-berlin.de>
2019-03-11 22:35:37 +0100

    multiprocessing sources, processing_steps sinks

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import logging, sys, time, unittest, os
-
-from bitflow.sink import FileSink, NoopSink, SendSink, StdSink, ListenSink
+from bitflow.sinksteps import *
+#from bitflow.sink import FileSink, NoopSink, SendSink, StdSink, ListenSink
 from bitflow.marshaller import CsvMarshaller
 from bitflow.pipeline import Pipeline
 from bitflow.source import FileSource, ListenSource, DownloadSource
@@ -21,34 +21,13 @@ def remove_file(f):
 
 class TestInputOutput(unittest.TestCase):
 
-    def test_file_in_noop_out(self):
+    def test_file_in_no_out(self):
         global LOGGING_LEVEL
         global TESTING_IN_FILE
         logging.basicConfig(format='%(asctime)s %(message)s', level=LOGGING_LEVEL)
 
-        noop_sink = NoopSink()
-        noop_sink.start()
-
         pipeline = Pipeline()
-        pipeline.set_sink([noop_sink])
-        pipeline.start()
-
-        file_source = FileSource(filename=TESTING_IN_FILE,pipeline=pipeline,marshaller=CsvMarshaller())    
-        file_source.start()
-
-    def test_file_in_multi_noop_out(self):
-        global LOGGING_LEVEL
-        global TESTING_IN_FILE
-        logging.basicConfig(format='%(asctime)s %(message)s', level=LOGGING_LEVEL)
-
-        noop_sink1 = NoopSink()
-        noop_sink1.start()
-
-        noop_sink2 = NoopSink()
-        noop_sink2.start()
-
-        pipeline = Pipeline()
-        pipeline.set_sink([noop_sink1,noop_sink2])
+        #pipeline.add_processing_step(TerminalOut())
         pipeline.start()
 
         file_source = FileSource(filename=TESTING_IN_FILE,pipeline=pipeline,marshaller=CsvMarshaller())    
@@ -61,11 +40,9 @@ class TestInputOutput(unittest.TestCase):
 
         remove_file(TESTING_OUT_FILE)
 
-        file_sink = FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE)
-        file_sink.start()
-
         pipeline = Pipeline()
-        pipeline.set_sink([file_sink])
+        pipeline.add_processing_step(FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE))
+
         pipeline.start()
 
         file_source = FileSource(filename=TESTING_IN_FILE,pipeline=pipeline,marshaller=CsvMarshaller())    
@@ -83,16 +60,13 @@ class TestInputOutput(unittest.TestCase):
         remove_file(TESTING_OUT_FILE)
         remove_file(TESTING_OUT_FILE_2)
 
-        file_sink1 = FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE)
-        file_sink2 = FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE_2)
-        file_sink1.start()
-        file_sink2.start()
-
         pipeline = Pipeline()
-        pipeline.set_sink([file_sink1,file_sink2])
+        pipeline.add_processing_step(FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE))
+        pipeline.add_processing_step(FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE_2))
         pipeline.start()
 
         file_source = FileSource(filename=TESTING_IN_FILE,pipeline=pipeline,marshaller=CsvMarshaller())    
+
         file_source.start()
 
         time.sleep(2.0)
@@ -102,34 +76,44 @@ class TestInputOutput(unittest.TestCase):
     def test_listen_in_send_out(self):
         global LOGGING_LEVEL
         global TESTING_OUT_FILE, TESTING_IN_FILE
+       
         logging.basicConfig(format='%(asctime)s %(message)s', level=LOGGING_LEVEL)
         remove_file(TESTING_OUT_FILE)
+       
         host="localhost"
-        port=5010
+        port=5011
 
         # BUILD LISTEN TO FILE
-        a_file_sink = FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE)
         a_pipeline = Pipeline()
-        a_file_sink.start()
-        a_pipeline.set_sink([a_file_sink])
-        a_listen_source = ListenSource(marshaller=CsvMarshaller(),pipeline=a_pipeline,host=host,port=port)
+        a_pipeline.add_processing_step(FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE))
+        a_listen_source = ListenSource(
+                        marshaller=CsvMarshaller(),
+                        pipeline=a_pipeline,
+                        host=host,
+                        port=port)
         a_pipeline.start()
         a_listen_source.start()
 
+        time.sleep(2)
         # BUILD FILE TO SEND
-        b_send_sink = SendSink(marshaller=CsvMarshaller(),host=host,port=port)
         b_pipeline = Pipeline()
-        b_pipeline.set_sink([b_send_sink])
+        b_pipeline.add_processing_step(TCPSink(
+                            marshaller=CsvMarshaller(),
+                            host=host,
+                            port=port))
         b_file_source = FileSource(filename=TESTING_IN_FILE,pipeline=b_pipeline,marshaller=CsvMarshaller())    
 
-        b_send_sink.start()
         b_pipeline.start()
         b_file_source.start()
 
+        time.sleep(5)
+
+        b_file_source.stop()
         time.sleep(2)
 
         a_listen_source.stop()
-        time.sleep(2)
+
+        #   time.sleep(5)
 
         import filecmp
         self.assertTrue(filecmp.cmp(TESTING_IN_FILE,TESTING_OUT_FILE))
@@ -156,6 +140,7 @@ class TestInputOutput(unittest.TestCase):
         b_pipeline = Pipeline()
         b_download_source = DownloadSource(marshaller=CsvMarshaller(),host=host,port=port,pipeline=b_pipeline)
         b_file_sink = FileSink(marshaller=CsvMarshaller(),filename=TESTING_OUT_FILE)
+        
         b_file_sink.start()
         b_pipeline.set_sink([b_file_sink])
         b_pipeline.start()
