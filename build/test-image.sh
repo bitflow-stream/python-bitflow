ccc7993296de1142996697fb50018e0b5ea269fb
Anton Gulenko <anton.gulenko@googlemail.com>
2020-04-12 20:31:08 +0200

    Fixed test image script

--- build/test-image.sh
+++ build/test-image.sh
@@ -7,4 +7,4 @@ TAG="$1"
 # Sanity check: image starts, outputs valid JSON, and terminates. Empty output also results in a non-zero exit status of jq.
 2>&1 docker run "$IMAGE:$TAG" 'closed://- -> python(step=echo, args={msg="hello world"})' |\
     tee /dev/stderr |\
-    grep '[python/echo] hello world' > /dev/null
+    grep '\[python/echo\] hello world' > /dev/null
