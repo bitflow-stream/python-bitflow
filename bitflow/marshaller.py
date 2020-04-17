beadca08686dd2945e6edd97aeaaaae115b0afda
Alexander Acker <alexander.acker@tu-berlin.de>
2020-04-18 00:25:11 +0200

    Fixed time stamp marshalling.

--- bitflow/marshaller.py
+++ bitflow/marshaller.py
@@ -131,7 +131,7 @@ class BinaryMarshaller:
     # Formatting and sending samples and headers
     # ==========================================
 
-    epoch = datetime.datetime.fromtimestamp(0)
+    #epoch = datetime.datetime.fromtimestamp(0)
 
     def write_sample(self, stream, sample):
         stream.write(SAMPLE_MARKER_BYTE)
@@ -148,7 +148,7 @@ class BinaryMarshaller:
         stream.write(SEPARATOR_BYTE)
 
     def get_utc_nanos_timestamp(self, sample):
-        delta = sample.get_timestamp() - self.epoch
+        delta = sample.get_timestamp()
         return int(delta.total_seconds() * 1000000000)  # Nanoseconds, rounded to microseconds
 
     def format_tags(self, sample):
